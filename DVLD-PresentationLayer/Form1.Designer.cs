39537595c2bb4c7ee2330c025cb4b04a254e3e3b
ridabouakaz <bouakaz99@gmail.com>
2025-07-03 02:09:04 +0100

    Replace Photos and size him

--- DVLD-PresentationLayer/Form1.Designer.cs
+++ DVLD-PresentationLayer/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace DVLD_PresentationLayer
+﻿using System.Windows.Forms;
+
+namespace DVLD_PresentationLayer
 {
     partial class FOmain
     {
@@ -31,8 +33,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FOmain));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.SMItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.SMItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.hiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SMItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,7 +50,7 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(10, 20, 10, 20);
-            this.menuStrip1.Size = new System.Drawing.Size(1087, 81);
+            this.menuStrip1.Size = new System.Drawing.Size(1087, 556);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -57,21 +59,22 @@
             this.SMItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.hiToolStripMenuItem});
             this.SMItem1.Image = ((System.Drawing.Image)(resources.GetObject("SMItem1.Image")));
+             this.SMItem1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.SizeToFit;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(64, 64); // حجم متوسط
             this.SMItem1.Name = "SMItem1";
-            this.SMItem1.Size = new System.Drawing.Size(200, 41);
             this.SMItem1.Text = "Application";
-            // 
-            // SMItem2
-            // 
-            this.SMItem2.Name = "SMItem2";
-            this.SMItem2.Size = new System.Drawing.Size(120, 41);
-            this.SMItem2.Text = "people";
             // 
             // hiToolStripMenuItem
             // 
             this.hiToolStripMenuItem.Name = "hiToolStripMenuItem";
-            this.hiToolStripMenuItem.Size = new System.Drawing.Size(224, 42);
+            this.hiToolStripMenuItem.Size = new System.Drawing.Size(132, 42);
             this.hiToolStripMenuItem.Text = "hi";
+            // 
+            // SMItem2
+            // 
+            this.SMItem2.Name = "SMItem2";
+            this.SMItem2.Size = new System.Drawing.Size(120, 516);
+            this.SMItem2.Text = "people";
             // 
             // FOmain
             // 
