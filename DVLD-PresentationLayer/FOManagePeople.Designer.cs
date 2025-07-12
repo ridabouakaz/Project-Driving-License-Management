4a213d87f63022a172ebbbc97c33507499c25e70
ridabouakaz <bouakaz99@gmail.com>
2025-07-12 02:23:37 +0100

    add Data Grid View to show information people

--- DVLD-PresentationLayer/FOManagePeople.Designer.cs
+++ DVLD-PresentationLayer/FOManagePeople.Designer.cs
@@ -28,20 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dGViewShowInformation = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dGViewShowInformation)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dGViewShowInformation
+            // 
+            this.dGViewShowInformation.AllowUserToAddRows = false;
+            this.dGViewShowInformation.AllowUserToDeleteRows = false;
+            this.dGViewShowInformation.AllowUserToOrderColumns = true;
+            this.dGViewShowInformation.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dGViewShowInformation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGViewShowInformation.Location = new System.Drawing.Point(177, 245);
+            this.dGViewShowInformation.Name = "dGViewShowInformation";
+            this.dGViewShowInformation.ReadOnly = true;
+            this.dGViewShowInformation.RowHeadersWidth = 51;
+            this.dGViewShowInformation.RowTemplate.Height = 24;
+            this.dGViewShowInformation.Size = new System.Drawing.Size(1233, 409);
+            this.dGViewShowInformation.TabIndex = 0;
             // 
             // FOManagePeople
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1662, 666);
+            this.Controls.Add(this.dGViewShowInformation);
             this.Name = "FOManagePeople";
             this.ShowIcon = false;
             this.Text = "Manage People";
+            this.Load += new System.EventHandler(this.FOManagePeople_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dGViewShowInformation)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dGViewShowInformation;
     }
 }
