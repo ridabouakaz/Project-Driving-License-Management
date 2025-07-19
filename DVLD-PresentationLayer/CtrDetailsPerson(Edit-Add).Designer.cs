faeb9d938df5510bb81aaa482904cfbd3f4dc538
ridabouakaz <bouakaz99@gmail.com>
2025-07-19 16:57:29 +0100

    Adding More to Picture and TextBoxs

--- DVLD-PresentationLayer/CtrDetailsPerson(Edit-Add).Designer.cs
+++ DVLD-PresentationLayer/CtrDetailsPerson(Edit-Add).Designer.cs
@@ -41,14 +41,25 @@ namespace DVLD_PresentationLayer
             this.LblSecondName = new System.Windows.Forms.Label();
             this.LblThridName = new System.Windows.Forms.Label();
             this.LblLastName = new System.Windows.Forms.Label();
-            this.pBName = new System.Windows.Forms.PictureBox();
-            this.pBNationalNo = new System.Windows.Forms.PictureBox();
-            this.pBEmail = new System.Windows.Forms.PictureBox();
-            this.pBAddress = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pBName)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pBNationalNo)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pBEmail)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pBAddress)).BeginInit();
+            this.PBName = new System.Windows.Forms.PictureBox();
+            this.PBNationalNo = new System.Windows.Forms.PictureBox();
+            this.PBEmail = new System.Windows.Forms.PictureBox();
+            this.PBAddress = new System.Windows.Forms.PictureBox();
+            this.PBMan = new System.Windows.Forms.PictureBox();
+            this.plGender = new System.Windows.Forms.Panel();
+            this.TBFirstName = new System.Windows.Forms.TextBox();
+            this.TBSecondName = new System.Windows.Forms.TextBox();
+            this.TBThridName = new System.Windows.Forms.TextBox();
+            this.TBLastName = new System.Windows.Forms.TextBox();
+            this.TBNationalNo = new System.Windows.Forms.TextBox();
+            this.TBEmail = new System.Windows.Forms.TextBox();
+            this.TBAddress = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PBName)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBNationalNo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBEmail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBAddress)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBMan)).BeginInit();
+            this.plGender.SuspendLayout();
             this.SuspendLayout();
             // 
             // LblName
@@ -117,7 +128,7 @@ namespace DVLD_PresentationLayer
             this.LblFirstName.BackColor = System.Drawing.Color.Transparent;
             this.LblFirstName.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.LblFirstName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
-            this.LblFirstName.Location = new System.Drawing.Point(236, 29);
+            this.LblFirstName.Location = new System.Drawing.Point(243, 29);
             this.LblFirstName.Name = "LblFirstName";
             this.LblFirstName.Size = new System.Drawing.Size(41, 23);
             this.LblFirstName.TabIndex = 5;
@@ -129,7 +140,7 @@ namespace DVLD_PresentationLayer
             this.LblSecondName.BackColor = System.Drawing.Color.Transparent;
             this.LblSecondName.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.LblSecondName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
-            this.LblSecondName.Location = new System.Drawing.Point(403, 29);
+            this.LblSecondName.Location = new System.Drawing.Point(406, 29);
             this.LblSecondName.Name = "LblSecondName";
             this.LblSecondName.Size = new System.Drawing.Size(66, 23);
             this.LblSecondName.TabIndex = 6;
@@ -141,7 +152,7 @@ namespace DVLD_PresentationLayer
             this.LblThridName.BackColor = System.Drawing.Color.Transparent;
             this.LblThridName.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.LblThridName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
-            this.LblThridName.Location = new System.Drawing.Point(595, 29);
+            this.LblThridName.Location = new System.Drawing.Point(590, 29);
             this.LblThridName.Name = "LblThridName";
             this.LblThridName.Size = new System.Drawing.Size(49, 23);
             this.LblThridName.TabIndex = 7;
@@ -153,47 +164,142 @@ namespace DVLD_PresentationLayer
             this.LblLastName.BackColor = System.Drawing.Color.Transparent;
             this.LblLastName.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.LblLastName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
-            this.LblLastName.Location = new System.Drawing.Point(770, 29);
+            this.LblLastName.Location = new System.Drawing.Point(768, 29);
             this.LblLastName.Name = "LblLastName";
             this.LblLastName.Size = new System.Drawing.Size(40, 23);
             this.LblLastName.TabIndex = 8;
             this.LblLastName.Text = "Last";
             // 
-            // pBName
+            // PBName
             // 
-            this.pBName.Image = ((System.Drawing.Image)(resources.GetObject("pBName.Image")));
-            this.pBName.Location = new System.Drawing.Point(157, 66);
-            this.pBName.Name = "pBName";
-            this.pBName.Size = new System.Drawing.Size(24, 24);
-            this.pBName.TabIndex = 9;
-            this.pBName.TabStop = false;
+            this.PBName.Image = ((System.Drawing.Image)(resources.GetObject("PBName.Image")));
+            this.PBName.Location = new System.Drawing.Point(157, 66);
+            this.PBName.Name = "PBName";
+            this.PBName.Size = new System.Drawing.Size(24, 24);
+            this.PBName.TabIndex = 9;
+            this.PBName.TabStop = false;
             // 
-            // pBNationalNo
+            // PBNationalNo
             // 
-            this.pBNationalNo.Image = ((System.Drawing.Image)(resources.GetObject("pBNationalNo.Image")));
-            this.pBNationalNo.Location = new System.Drawing.Point(157, 110);
-            this.pBNationalNo.Name = "pBNationalNo";
-            this.pBNationalNo.Size = new System.Drawing.Size(24, 24);
-            this.pBNationalNo.TabIndex = 10;
-            this.pBNationalNo.TabStop = false;
+            this.PBNationalNo.Image = ((System.Drawing.Image)(resources.GetObject("PBNationalNo.Image")));
+            this.PBNationalNo.Location = new System.Drawing.Point(157, 110);
+            this.PBNationalNo.Name = "PBNationalNo";
+            this.PBNationalNo.Size = new System.Drawing.Size(24, 24);
+            this.PBNationalNo.TabIndex = 10;
+            this.PBNationalNo.TabStop = false;
             // 
-            // pBEmail
+            // PBEmail
             // 
-            this.pBEmail.Image = ((System.Drawing.Image)(resources.GetObject("pBEmail.Image")));
-            this.pBEmail.Location = new System.Drawing.Point(157, 198);
-            this.pBEmail.Name = "pBEmail";
-            this.pBEmail.Size = new System.Drawing.Size(24, 24);
-            this.pBEmail.TabIndex = 11;
-            this.pBEmail.TabStop = false;
+            this.PBEmail.Image = ((System.Drawing.Image)(resources.GetObject("PBEmail.Image")));
+            this.PBEmail.Location = new System.Drawing.Point(157, 198);
+            this.PBEmail.Name = "PBEmail";
+            this.PBEmail.Size = new System.Drawing.Size(24, 24);
+            this.PBEmail.TabIndex = 11;
+            this.PBEmail.TabStop = false;
             // 
-            // pBAddress
+            // PBAddress
             // 
-            this.pBAddress.Image = ((System.Drawing.Image)(resources.GetObject("pBAddress.Image")));
-            this.pBAddress.Location = new System.Drawing.Point(157, 242);
-            this.pBAddress.Name = "pBAddress";
-            this.pBAddress.Size = new System.Drawing.Size(24, 24);
-            this.pBAddress.TabIndex = 12;
-            this.pBAddress.TabStop = false;
+            this.PBAddress.Image = ((System.Drawing.Image)(resources.GetObject("PBAddress.Image")));
+            this.PBAddress.Location = new System.Drawing.Point(157, 242);
+            this.PBAddress.Name = "PBAddress";
+            this.PBAddress.Size = new System.Drawing.Size(24, 24);
+            this.PBAddress.TabIndex = 12;
+            this.PBAddress.TabStop = false;
+            // 
+            // PBMan
+            // 
+            this.PBMan.Image = ((System.Drawing.Image)(resources.GetObject("PBMan.Image")));
+            this.PBMan.Location = new System.Drawing.Point(0, 0);
+            this.PBMan.Name = "PBMan";
+            this.PBMan.Size = new System.Drawing.Size(24, 24);
+            this.PBMan.TabIndex = 13;
+            this.PBMan.TabStop = false;
+            // 
+            // plGender
+            // 
+            this.plGender.Controls.Add(this.PBMan);
+            this.plGender.Location = new System.Drawing.Point(157, 154);
+            this.plGender.Name = "plGender";
+            this.plGender.Size = new System.Drawing.Size(188, 24);
+            this.plGender.TabIndex = 14;
+            // 
+            // TBFirstName
+            // 
+            this.TBFirstName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(247)))));
+            this.TBFirstName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TBFirstName.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TBFirstName.ForeColor = System.Drawing.Color.Black;
+            this.TBFirstName.Location = new System.Drawing.Point(191, 65);
+            this.TBFirstName.Name = "TBFirstName";
+            this.TBFirstName.Size = new System.Drawing.Size(154, 27);
+            this.TBFirstName.TabIndex = 15;
+            // 
+            // TBSecondName
+            // 
+            this.TBSecondName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(247)))));
+            this.TBSecondName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TBSecondName.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TBSecondName.ForeColor = System.Drawing.Color.Black;
+            this.TBSecondName.Location = new System.Drawing.Point(364, 65);
+            this.TBSecondName.Name = "TBSecondName";
+            this.TBSecondName.Size = new System.Drawing.Size(154, 27);
+            this.TBSecondName.TabIndex = 16;
+            // 
+            // TBThridName
+            // 
+            this.TBThridName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(247)))));
+            this.TBThridName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TBThridName.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TBThridName.ForeColor = System.Drawing.Color.Black;
+            this.TBThridName.Location = new System.Drawing.Point(539, 65);
+            this.TBThridName.Name = "TBThridName";
+            this.TBThridName.Size = new System.Drawing.Size(154, 27);
+            this.TBThridName.TabIndex = 17;
+            // 
+            // TBLastName
+            // 
+            this.TBLastName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(247)))));
+            this.TBLastName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TBLastName.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TBLastName.ForeColor = System.Drawing.Color.Black;
+            this.TBLastName.Location = new System.Drawing.Point(717, 65);
+            this.TBLastName.Name = "TBLastName";
+            this.TBLastName.Size = new System.Drawing.Size(154, 27);
+            this.TBLastName.TabIndex = 18;
+            // 
+            // TBNationalNo
+            // 
+            this.TBNationalNo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(247)))));
+            this.TBNationalNo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TBNationalNo.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TBNationalNo.ForeColor = System.Drawing.Color.Black;
+            this.TBNationalNo.Location = new System.Drawing.Point(191, 109);
+            this.TBNationalNo.Name = "TBNationalNo";
+            this.TBNationalNo.Size = new System.Drawing.Size(154, 27);
+            this.TBNationalNo.TabIndex = 19;
+            // 
+            // TBEmail
+            // 
+            this.TBEmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(247)))));
+            this.TBEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TBEmail.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TBEmail.ForeColor = System.Drawing.Color.Black;
+            this.TBEmail.Location = new System.Drawing.Point(191, 197);
+            this.TBEmail.Name = "TBEmail";
+            this.TBEmail.Size = new System.Drawing.Size(154, 27);
+            this.TBEmail.TabIndex = 20;
+            // 
+            // TBAddress
+            // 
+            this.TBAddress.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(247)))));
+            this.TBAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TBAddress.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TBAddress.ForeColor = System.Drawing.Color.Black;
+            this.TBAddress.Location = new System.Drawing.Point(191, 241);
+            this.TBAddress.Multiline = true;
+            this.TBAddress.Name = "TBAddress";
+            this.TBAddress.Size = new System.Drawing.Size(502, 111);
+            this.TBAddress.TabIndex = 21;
             // 
             // CtrDetailsPerson_Edit_Add_
             // 
@@ -201,10 +307,18 @@ namespace DVLD_PresentationLayer
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(240)))), ((int)(((byte)(250)))));
             this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.Controls.Add(this.pBAddress);
-            this.Controls.Add(this.pBEmail);
-            this.Controls.Add(this.pBNationalNo);
-            this.Controls.Add(this.pBName);
+            this.Controls.Add(this.TBAddress);
+            this.Controls.Add(this.TBEmail);
+            this.Controls.Add(this.TBNationalNo);
+            this.Controls.Add(this.TBLastName);
+            this.Controls.Add(this.TBThridName);
+            this.Controls.Add(this.TBSecondName);
+            this.Controls.Add(this.TBFirstName);
+            this.Controls.Add(this.plGender);
+            this.Controls.Add(this.PBAddress);
+            this.Controls.Add(this.PBEmail);
+            this.Controls.Add(this.PBNationalNo);
+            this.Controls.Add(this.PBName);
             this.Controls.Add(this.LblLastName);
             this.Controls.Add(this.LblAddress);
             this.Controls.Add(this.LblEmail);
@@ -215,11 +329,13 @@ namespace DVLD_PresentationLayer
             this.Controls.Add(this.LblSecondName);
             this.Controls.Add(this.LblFirstName);
             this.Name = "CtrDetailsPerson_Edit_Add_";
-            this.Size = new System.Drawing.Size(896, 367);
-            ((System.ComponentModel.ISupportInitialize)(this.pBName)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pBNationalNo)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pBEmail)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pBAddress)).EndInit();
+            this.Size = new System.Drawing.Size(896, 388);
+            ((System.ComponentModel.ISupportInitialize)(this.PBName)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBNationalNo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBEmail)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBAddress)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBMan)).EndInit();
+            this.plGender.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -236,9 +352,18 @@ namespace DVLD_PresentationLayer
         private System.Windows.Forms.Label LblSecondName;
         private System.Windows.Forms.Label LblThridName;
         private System.Windows.Forms.Label LblLastName;
-        private System.Windows.Forms.PictureBox pBName;
-        private PictureBox pBNationalNo;
-        private PictureBox pBEmail;
-        private PictureBox pBAddress;
+        private System.Windows.Forms.PictureBox PBName;
+        private PictureBox PBNationalNo;
+        private PictureBox PBEmail;
+        private PictureBox PBAddress;
+        private PictureBox PBMan;
+        private Panel plGender;
+        private TextBox TBFirstName;
+        private TextBox TBSecondName;
+        private TextBox TBThridName;
+        private TextBox TBLastName;
+        private TextBox TBNationalNo;
+        private TextBox TBEmail;
+        private TextBox TBAddress;
     }
 }
