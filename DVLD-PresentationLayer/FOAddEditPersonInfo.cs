bff2989f8787a64b8accf374015018b3f5a08274
ridabouakaz <bouakaz99@gmail.com>
2025-07-27 02:34:09 +0100

    Start to class to Country and prepare Tier

--- DVLD-PresentationLayer/FOAddEditPersonInfo.cs
+++ DVLD-PresentationLayer/FOAddEditPersonInfo.cs
@@ -12,9 +12,32 @@ namespace DVLD_PresentationLayer
 {
     public partial class FOAddEditPersonInfo : Form
     {
-        public FOAddEditPersonInfo()
+
+        public enum enMode { AddNew = 0, Update = 1 };
+        private enMode _Mode;
+        int _ContactID;
+        clsContact _Contact;
+        public FOAddEditPersonInfo(int ContactID)
         {
             InitializeComponent();
+            _ContactID = ContactID;
+
+            if (_ContactID == -1)
+                _Mode = enMode.AddNew;
+            else
+                _Mode = enMode.Update;
+        }
+        private void _FillCountriesInComoboBox()
+        {
+            DataTable dtCountries = clsCountry.GetAllCountries();
+
+            foreach (DataRow row in dtCountries.Rows)
+            {
+
+                cbCountry.Items.Add(row["CountryName"]);
+
+            }
+
         }
         private void FOAddEditPersonInfo_Load(object sender, EventArgs e)
         {
