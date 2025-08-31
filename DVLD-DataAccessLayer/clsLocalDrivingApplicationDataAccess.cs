2e2169651fa28d3255d1fc490a91b9a5ec2b6791
ridabouakaz <bouakaz99@gmail.com>
2025-08-31 16:29:36 +0100

    [clsLocalDrivingApplicationDataAccess]--Adding Function Add new Local Driving Application

--- DVLD-DataAccessLayer/clsLocalDrivingApplicationDataAccess.cs
+++ DVLD-DataAccessLayer/clsLocalDrivingApplicationDataAccess.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DVLD_DataAccessLayer
 {
-    internal class clsLocalDrivingApplicationDataAccess
+    public class clsLocalDrivingApplicationDataAccess
     {
+        public static int AddNewLocalDrivingApplication(
+    int ApplicationID, int LicenseClassID)
+        {
+            int NewLocalDrivingApplicationID = -1;
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                string query = @"INSERT INTO LocalDrivingLicenseApplications 
+                        (ApplicationID, LicenseClassID)
+                        VALUES 
+                        (@ApplicationID, @LicenseClassID);
+                        SELECT SCOPE_IDENTITY();";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ApplicationID", ApplicationID);
+                    command.Parameters.AddWithValue("@LicenseClassID", LicenseClassID);
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            NewLocalDrivingApplicationID = insertedID;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error adding new Application: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            return NewLocalDrivingApplicationID;
+        }
+
+
+
     }
 }
