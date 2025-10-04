3e9795facf3229c0bcac7139c79465f20b6aa485
ridabouakaz <bouakaz99@gmail.com>
2025-10-04 03:24:53 +0100

    [clsDrivers][clsDriverDataAccess]--adding New Class for hundle Drivers Applications

---
+++ DVLD-BusinessLayer/clsDrivers.cs
@@ -0,0 +1,48 @@
+﻿using DVLD_DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DVLDShared.DVLDShared;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DVLD_BusinessLayer
+{
+    public class clsDrivers
+    {
+        public int DriverID { set; get; }
+        public int PersonID { set; get; }
+        public int CreatedByUserID { set; get; }
+        public DateTime CreatedDate { set; get; }
+
+        public clsDrivers()
+        {
+            DriverID = -1;
+            PersonID = 0;
+            CreatedByUserID = 0;
+            CreatedDate = DateTime.Today;
+        }
+
+        private clsDrivers(
+       int DriverID,
+       int PersonID, int CreatedByUserID,
+       DateTime CreatedDate)
+        {
+            this.DriverID = DriverID;
+            this.PersonID = PersonID;
+            this.CreatedByUserID = CreatedByUserID;
+            this.CreatedDate = CreatedDate;
+        }
+        private bool _AddDriver()
+        {
+            //call DataAccess Layer 
+            this.DriverID = clsDriverDataAccess.AddNewDriver(
+        this.PersonID,
+         this.CreatedByUserID,
+        this.CreatedDate
+             );
+            return (this.DriverID != -1);
+        }
+    }
+}
---
+++ DVLD-DataAccessLayer/clsDriverDataAccess.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static DVLDShared.DVLDShared;
+
+namespace DVLD_DataAccessLayer
+{
+    public class clsDriverDataAccess
+    {
+        public static int AddNewDriver(
+  int PersonID, int CreatedByUserID, DateTime CreatedDate)
+        {
+            int DriverID = -1;
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                string query = @"INSERT INTO drivers 
+                        (PersonID, CreatedByUserID, CreatedDate)
+                        VALUES 
+                        (@PersonID, @CreatedByUserID, @CreatedDate);
+                        SELECT SCOPE_IDENTITY();";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@PersonID", PersonID);
+                    command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+                    command.Parameters.AddWithValue("@CreatedDate", CreatedDate);
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            DriverID = insertedID;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error adding new Application: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            return DriverID;
+        }
+
+
+        AddNewDriver()
+        {
+
+
+
+
+
+
+        }
+
+    }
+}
